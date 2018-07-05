92d4114b92b816da29b0bb49803c77c5881ea667
shiweibsw <shiweibsw@gmail.com>
2018-07-05 16:00:08 +0800

    ActivityExtends

---
+++ app/src/main/java/com/kd/kotlin/extend/utils/ActivityExtends.kt
@@ -0,0 +1,28 @@
+package com.kd.kotlin.extend.utils
+
+import android.app.Activity
+import android.graphics.Bitmap
+import android.util.DisplayMetrics
+
+/**
+ * 屏幕截图
+ */
+fun Activity.screenShot(activity: Activity, isDeleteStatusBar: Boolean = true): Bitmap {
+    val decorView = activity.window.decorView
+    decorView.isDrawingCacheEnabled = true
+    decorView.buildDrawingCache()
+    val bmp = decorView.drawingCache
+    val dm = DisplayMetrics()
+    activity.windowManager.defaultDisplay.getMetrics(dm)
+    var ret: Bitmap? = null
+    if (isDeleteStatusBar) {
+        val resources = activity.resources
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        val statusBarHeight = resources.getDimensionPixelSize(resourceId)
+        ret = Bitmap.createBitmap(bmp, 0, statusBarHeight, dm.widthPixels, dm.heightPixels - statusBarHeight)
+    } else {
+        ret = Bitmap.createBitmap(bmp, 0, 0, dm.widthPixels, dm.heightPixels)
+    }
+    decorView.destroyDrawingCache()
+    return ret!!
+}
