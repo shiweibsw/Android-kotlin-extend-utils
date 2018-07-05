a4a6f857c893c5bc65750639c67e04b535e0de59
shiweibsw <shiweibsw@gmail.com>
2018-07-05 15:07:32 +0800

    SharedPreferencesExtends

--- app/src/main/java/com/kd/kotlin/extend/utils/ContextExtends.kt
+++ app/src/main/java/com/kd/kotlin/extend/utils/ContextExtends.kt
@@ -3,39 +3,92 @@ package com.kd.kotlin.extend.utils
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Point
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.os.Build
 import android.view.Gravity
+import android.view.WindowManager
 import android.widget.Toast
 
 
-
 //----------toast----------
-fun Context.toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT){
-    Toast.makeText(this,text,duration).show()
+fun Context.toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, text, duration).show()
 }
-fun Context.toast(resId: Int, duration: Int = Toast.LENGTH_SHORT){
-    Toast.makeText(this,resId,duration).show()
+
+fun Context.toast(resId: Int, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, resId, duration).show()
 }
-fun Context.centerToast(resId: Int, duration: Int = Toast.LENGTH_SHORT){
-    var t=Toast.makeText(this,resId,duration)
-    t.setGravity(Gravity.CENTER,0,0)
+
+fun Context.centerToast(resId: Int, duration: Int = Toast.LENGTH_SHORT) {
+    var t = Toast.makeText(this, resId, duration)
+    t.setGravity(Gravity.CENTER, 0, 0)
     t.show()
 }
 
+//----------尺寸转换----------
+
+fun Context.dp2px(dpValue: Float): Int {
+    val scale = resources.displayMetrics.density
+    return (dpValue * scale + 0.5f).toInt()
+}
+
+fun Context.px2dp(pxValue: Float): Int {
+    val scale = resources.displayMetrics.density
+    return (pxValue / scale + 0.5f).toInt()
+}
+
+fun Context.sp2px(spValue: Float): Int {
+    val scale = resources.displayMetrics.scaledDensity
+    return (spValue * scale + 0.5f).toInt()
+}
+
+fun Context.px2sp(pxValue: Float): Int {
+    val scale = resources.displayMetrics.scaledDensity
+    return (pxValue / scale + 0.5f).toInt()
+}
+
+//----------屏幕尺寸----------
+
+fun Context.getScreenWidth(): Int {
+    var wm: WindowManager = this.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            ?: return resources.displayMetrics.widthPixels
+    var point = Point()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        wm.defaultDisplay.getRealSize(point)
+    } else {
+        wm.defaultDisplay.getSize(point)
+    }
+    return point.x
+}
+
+fun Context.getScreenHeight(): Int {
+    var wm: WindowManager = this.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            ?: return resources.displayMetrics.heightPixels
+    var point = Point()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        wm.defaultDisplay.getRealSize(point)
+    } else {
+        wm.defaultDisplay.getSize(point)
+    }
+    return point.y
+}
+
 //----------NetWork----------
+
 /**
  * 打开网络设置
  */
-fun Context.openWirelessSettings(){
+fun Context.openWirelessSettings() {
     startActivity(Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
 }
 
 /**
  * 网络是否连接
  */
-fun Context.isConnected():Boolean{
-   var info = this.getActiveNetworkInfo()
+fun Context.isConnected(): Boolean {
+    var info = this.getActiveNetworkInfo()
     return info.isConnected
 }
 
--- app/src/main/java/com/kd/kotlin/extend/utils/MainActivity.kt
+++ app/src/main/java/com/kd/kotlin/extend/utils/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         img.loadImage(this,"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530504284670&di=290348ed26686c9abf5ecc8274f86271&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fb7003af33a87e95053e42ae21c385343faf2b449.jpg")
         img.setOnClickListener {
-            centerToast(R.string.app_name)
+            toast(px2dp(48.0f).toString())
         }
     }
 }
