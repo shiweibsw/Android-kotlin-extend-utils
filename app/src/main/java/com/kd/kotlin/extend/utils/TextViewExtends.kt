0dd78149f0b23433d4764f117162b88321a5b3c6
shiweibsw <shiweibsw@gmail.com>
2018-07-09 10:34:20 +0800

    TextViewExtends

--- app/src/main/java/com/kd/kotlin/extend/utils/TextViewExtends.kt
+++ app/src/main/java/com/kd/kotlin/extend/utils/TextViewExtends.kt
@@ -9,3 +9,27 @@ fun TextView.setColor(resId: Int) {
     this.setTextColor(resources.getColor(resId))
 }
 
+fun TextView.setDrawableLeft(resId: Int) {
+    var drawable = this.context.resources.getDrawable(resId)
+    drawable.setBounds(0, 0, drawable.minimumWidth, drawable.minimumHeight)
+    this.setCompoundDrawables(drawable, null, null, null)
+}
+
+fun TextView.setDrawableRight(resId: Int) {
+    var drawable = this.context.resources.getDrawable(resId)
+    drawable.setBounds(0, 0, drawable.minimumWidth, drawable.minimumHeight)
+    this.setCompoundDrawables(null, null, drawable, null)
+}
+
+fun TextView.setDrawableTop(resId: Int) {
+    var drawable = this.context.resources.getDrawable(resId)
+    drawable.setBounds(0, 0, drawable.minimumWidth, drawable.minimumHeight)
+    this.setCompoundDrawables(null, drawable, null, null)
+}
+
+fun TextView.setDrawableBottom(resId: Int) {
+    var drawable = this.context.resources.getDrawable(resId)
+    drawable.setBounds(0, 0, drawable.minimumWidth, drawable.minimumHeight)
+    this.setCompoundDrawables(null, null, null, drawable)
+}
+
